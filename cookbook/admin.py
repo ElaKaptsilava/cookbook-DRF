7931fa74fe82fdebbd57f60374b62eec73452b44
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-07-24 16:48:54 +0200

    modificate admin site

--- cookbook/admin.py
+++ cookbook/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Recipe, Instruction, Ingredient, IngredientRecipe, Category
 
-# Register your models here.
+admin.site.site_header = "CookBook Django"
+
+
+class InstructionsInline(admin.TabularInline):
+    model = Instruction
+    extra = 1
+
+
+class IngredientsInLine(admin.TabularInline):
+    model = IngredientRecipe
+    extra = 1
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [InstructionsInline, IngredientsInLine]
+    list_display = ["user", "category", "title", "published_date"]
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient)
+admin.site.register(Category)
+admin.site.register(IngredientRecipe)
