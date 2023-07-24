123fda032f2774f52121f42b3fbb3fb7446b0d02
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-07-24 16:41:45 +0200

    create serializers

--- cookbook/serialisers.py
+++ cookbook/serialisers.py
@@ -1,55 +1,37 @@
-from django.conf import settings
-from django.db import models
+from rest_framework import serializers
+
+from .models import Recipe, Category, IngredientRecipe, Ingredient, Instruction
 
 
-class BaseRecipe(models.Model):
-    image = models.ImageField(null=True, blank=True)
-    description = models.TextField()
+class CategorySerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class IngredientSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
+
+
+class RecipeSerializers(serializers.ModelSerializer):
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault()
+    )
 
     class Meta:
-        abstract = True
+        model = Recipe
+        fields = '__all__'
 
 
-class Recipe(BaseRecipe):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=300)
-    category = models.ForeignKey('Category', on_delete=models.PROTECT)
-    cooking_time = models.IntegerField()
-    ingredients = models.ManyToManyField("Ingredient", through='IngredientRecipe')
-    published_date = models.DateTimeField()
+class InstructionSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Instruction
+        fields = '__all__'
 
 
-class Instruction(BaseRecipe):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
-
-
-class IngredientRecipe(models.Model):
-    class MeasuringUnit(models.TextChoices):
-        milligram = 'mg'
-        gram = 'g'
-        kg = 'kg'
-        milliliter = 'ml'
-        liter = 'l'
-
-    weight = models.FloatField()
-    measuring_unit = models.CharField(max_length=10, choices=MeasuringUnit.choices, default=MeasuringUnit.gram)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    ingredient = models.ForeignKey('Ingredient', on_delete=models.CASCADE)
-
-
-class Ingredient(models.Model):
-    title = models.CharField(max_length=300)
-    calories = models.FloatField()
-    fat = models.FloatField(null=True, blank=True)
-    saturates = models.FloatField(null=True, blank=True)
-    carbs = models.FloatField(null=True, blank=True)
-
-    def __str__(self):
-        return self.title
+class IngredientRecipeSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = IngredientRecipe
+        fields = '__all__'
--- cookbook/views.py
+++ cookbook/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from .models import Recipe, Category, IngredientRecipe, Ingredient, Instruction
+from .serializers import RecipeSerializers, CategorySerializers, IngredientRecipeSerializers, IngredientSerializers, \
+    InstructionSerializers
 
-# Create your views here.
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializers
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializers
+    permission_classes = [IsAdminUser]
+
+
+class IngredientRecipeViewSet(viewsets.ModelViewSet):
+    queryset = IngredientRecipe.objects.all()
+    serializer_class = IngredientRecipeSerializers
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializers
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class InstructionViewSet(viewsets.ModelViewSet):
+    queryset = Instruction.objects.all()
+    serializer_class = InstructionSerializers
+    permission_classes = [IsAuthenticatedOrReadOnly]
