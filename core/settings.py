5dd3ab9705f4063434dfd3a3f386438fcc2de855
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-07-24 16:40:20 +0200

    create serializers

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cookbook.apps.CookbookConfig'
+    'cookbook.apps.CookbookConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
